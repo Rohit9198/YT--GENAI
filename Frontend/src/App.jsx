c938b32535a3654514ca73da652d00522adb0434
ROHIT SINGH <rohitsi2252@gmail.com>
2026-08-01 13:31:01 +0530

    2

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import {RouterProvider} from "react-router"
+import {router} from "./app.routes.jsx"
+import { AuthProvider} from "./features/auth/auth.context.jsx"
+
+function App() {
+ 
+
+  return (
+    <AuthProvide>
+    <RouterProvider router={router} />
+    </AuthProvide>
+  )
+}
+
+export default App
